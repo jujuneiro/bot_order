4c582be44f7ba21b0a6f32ee8ecccb02e666af64
jujuneiro <gameacc.kgs@gmail.com>
2025-05-21 15:19:48 +0300

    Add files via upload

--- internal/HTTP/http.go
+++ internal/HTTP/http.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"os"
 	"os/signal"
@@ -26,19 +27,24 @@ func http() {
 	router := gin.Default()
 
 	router.POST("/user/create-order", func(c *gin.Context) {
-		data := &plenka_bot.Data{}
+		data := &plenka_bot.Order{}
 		//bind автоматом ставит 400.
-		err = c.Bind(&data)
+		err = c.Bind(data)
 		if err != nil {
 			c.JSON(400, gin.H{"message": "неверные данные"})
 			return
 		}
-		b.SendMessageToChannel(ctx, data.Phone, data.Address, data.Comment)
-		if err != nil {
-			return
-		}
-	})
 
+		message := fmt.Sprintf("Имя: %s\nТелефон: %s\nEmail: %s\n", data.Name, data.Phone, data.Email)
+		for _, item := range data.Cart {
+			message += fmt.Sprintf("Продукт: %s\n", item.Product)
+			for _, variant := range item.Variants {
+				message += fmt.Sprintf("Цвет: %s, Количество: %d\n", variant.Color, variant.Count)
+			}
+		}
+		b.SendMessageToChannel(ctx, message)
+
+	})
 	router.Run("localhost:8081")
 	go b.Start(ctx)
 
